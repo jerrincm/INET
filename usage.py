c9c12f63b116ba078af95078609aa2dd13e930b5
jerrincm <erpsupport@novasoftindia.com>
2014-11-27 15:35:11 +0530

    Fixed error

--- usage.py
+++ usage.py
@@ -1,5 +1,10 @@
-from openerp.osv import osv
-from openerp.osv import fields
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+import time
+from openerp.osv import fields, osv
+from openerp.tools.translate import _
+from openerp.tools import DEFAULT_SERVER_DATE_FORMAT, DEFAULT_SERVER_DATETIME_FORMAT
+
 
 class net_usage(osv.osv):
 
@@ -12,6 +17,21 @@ class net_usage(osv.osv):
             
                 x[record.id]= record.ncs_download + record.ncs_upload
         return x
+    
+   
+    
+    def onchange_return_date(self, cr, uid, ids, issuedate, returndate):
+        a=False
+        b=False
+        total= False
+        if issuedate and returndate:
+            a=datetime.strptime(issuedate,"%Y-%m-%d %H:%M:%S")
+            b=datetime.strptime(returndate,"%Y-%m-%d %H:%M:%S")        
+            hour = relativedelta(b, a).hours
+            minutes = relativedelta(b, a).minutes
+            seconds = relativedelta(b, a).seconds
+            total = ("%s:%s:%s")%(hour,minutes,seconds)            
+        return {'value': {'ncs_duration':total}}
  
     _columns = {                       
                
